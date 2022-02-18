14414d6f9a82ee9806086c6dbe3ecef3874e10a4
Cristhofer Montalvo <cristhofermontalvo@gmail.com>
2022-02-18 14:52:35 -0500

    svg added

--- src/components/SkillsSection/Accordion/AccordionComponents.js
+++ src/components/SkillsSection/Accordion/AccordionComponents.js
@@ -1,80 +1,68 @@
-import styled,{css} from "styled-components";
-import {THEME} from "../../../theme"
+import styled, { css } from "styled-components";
+import { THEME } from "../../../theme";
 
 export const AccordionSkillsTypeList = styled.div`
-    display:flex;
-    flex-direction: column;
-`
+  display: flex;
+  flex-direction: column;
+`;
 
 export const AccordionSkillsTypeWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-`
+  display: flex;
+  flex-direction: column;
+`;
 export const SkillsListHeadingWraper = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    cursor: pointer;
-    border-bottom: 1px solid ${THEME.color.cyanBlue};
-`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  cursor: pointer;
+  border-bottom: 1px solid ${THEME.color.cyanBlue};
+`;
 
 export const SkillsListHeading = styled.h4`
-    font-size: ${THEME.font.size.h3};
-    letter-spacing: 0;
-    text-transform: uppercase;
-    margin: 0;
-    margin: 1.1rem 0;
-`
+  font-size: ${THEME.font.size.h3};
+  letter-spacing: 0;
+  text-transform: uppercase;
+  margin: 0;
+  margin: 1.1rem 0;
+`;
 export const SkillsListIcon = styled.div`
-    font-size: 30px;
-`
+  font-size: 30px;
+`;
 
 export const SkillsList = styled.div`
-    display: flex;
-    max-height: 0;
-    overflow: hidden;
-    flex-direction: column;
-    transition: all 0.5s cubic-bezier(0,1,0,1);
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(110px, 1fr));
+  max-height: 0;
+  row-gap: 1.5em;
+  overflow: hidden;
+  flex-direction: column;
+  transition: all 0.5s cubic-bezier(0, 1, 0, 1);
 
-    ${ props => props.isVisible && css`
-        max-height: 999px;
-        height: auto;
-        overflow: auto;
-        padding-bottom: 3rem;
+  ${(props) =>
+    props.isVisible &&
+    css`
+      max-height: 999px;
+      height: auto;
+      overflow: auto;
+      padding-bottom: 3rem;
+      padding-top: 3rem;
     `}
-`
+`;
+export const SkillImageWrapper = styled.div`
+  border: 1px solid ${THEME.color.cyanBlue};
+  width: 60px;
+  height: 60px;
+  padding: 0.75em;
+`;
 
-export const SkillWrapper = styled.div`
-    padding: 0.5rem 0;
-`
-export const SkillTitle = styled.p`
-    font-weight: 500;
-    font-size: 1.6rem;
-    text-transform: uppercase;
-`
-
-export const SkillProgress = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
-    position: relative;
-    width: 100%;
-    height: 20px;
-    background-color: ${THEME.color.cyanBlue};
-`
-
-export const SkillPaintedLine = styled.div`
-    width: ${(props) => props.percentage}%;
-    position: absolute;
-    height: 100%;
-    left: 0;
-    background-color:${THEME.color.accent};
-`
-
-export const SkillProcentage = styled.p`
-    padding: 0;
-    font-size: 14px;
-    padding-right: 0.2rem;
-    z-index: 1;
-    color : ${(props) => props.percentage === 100 ? '#fff' : '#000' }
-`
+export const SkillImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+  user-select: none;
+  ${({ theme, isInverted }) =>
+    !theme.isLight &&
+    css`
+      filter: ${isInverted ? "invert(100%)" : "invert(0%)"} opacity(0.8);
+    `}
+`;
---
+++ src/data/svg.js
@@ -0,0 +1,39 @@
+import html5 from "../images/tecnologies/html-5.svg";
+import css3 from "../images/tecnologies/css-3.svg";
+import javascript from "../images/tecnologies/javascript.svg";
+import react from "../images/tecnologies/react.svg";
+import alpinejs from "../images/tecnologies/alpinejs.svg";
+import nextjs from "../images/tecnologies/nextjs.svg";
+import wordpress from "../images/tecnologies/wordpress.svg";
+import python from "../images/tecnologies/python.svg";
+import laravel from "../images/tecnologies/laravel.svg";
+import php from "../images/tecnologies/php.svg";
+import nodejs from "../images/tecnologies/nodejs.svg";
+import tailwindcss from "../images/tecnologies/tailwindcss.svg";
+import livewire from "../images/tecnologies/livewire.png";
+import mysql from "../images/tecnologies/mysql.svg";
+import mongodb from "../images/tecnologies/mongodb.svg";
+import firebase from "../images/tecnologies/firebase.svg";
+import sqlserver from "../images/tecnologies/microsoft-sql-server.png";
+
+const SVGTecnologies = {
+  "html-5": html5,
+  "css-3": css3,
+  javascript,
+  react,
+  wordpress,
+  php,
+  laravel,
+  python,
+  nodejs,
+  alpinejs,
+  nextjs,
+  livewire,
+  tailwindcss,
+  mysql,
+  mongodb,
+  firebase,
+  "microsoft-sql-server": sqlserver,
+};
+
+export default SVGTecnologies;

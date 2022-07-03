0b51e82336a3df1e5ce544687b83780b357ae96e
Cristhofer Montalvo <cristhofermontalvo@gmail.com>
2022-07-03 18:08:02 -0500

    updated fonts

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -1,20 +1,18 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
-  *{
-    font-family: "FuturaPT";
-    letter-spacing: 0.05em;
-  }
+
 
   body {
       margin: 0;
-      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      font-family: "FuturaPT",-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
           'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
           sans-serif;
       -webkit-font-smoothing: antialiased;
       -moz-osx-font-smoothing: grayscale;
       background: ${({ theme }) => theme.body};
       color: ${({ theme }) => theme.text};
+      letter-spacing: 0.05em;
   }
 
   code {
